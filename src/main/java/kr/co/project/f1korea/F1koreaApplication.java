ccf80470bc9b5039a3c2c7905a6f01f3e1925fce
holelung <junho335276@gmail.com>
2023-11-09 14:26:44 +0900

    first

---
+++ src/main/java/kr/co/project/f1korea/F1koreaApplication.java
@@ -0,0 +1,13 @@
+package kr.co.project.f1korea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class F1koreaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(F1koreaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/project/f1korea/F1koreaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.project.f1korea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1koreaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
